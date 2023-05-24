a81e8471cfb5ae70d161cf8309c7e0d9531b7f2e
Tyshe-81 <adeniyioluwatishe@gmail.com>
2023-05-24 00:30:44 -0700

    hi

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -3,43 +3,51 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class SavingsAccount extends Accounts{
+public class SavingsAccount extends Accounts {
     private static final double WITHDRAWAL_LIMIT = 20000.0;
-private double balance;
+    private double balance;
 
     private final JButton savingsButton;
     private JButton SavingsButtonButton;
 
 
-        public SavingsAccount() {
-            super();
-            balance = 100000.0;
+    public SavingsAccount() {
+        super();
+        balance = 100000.0;
 
 
+        JLabel myAccountTypeLabel = new JLabel("Pick Your Account Type");
+        myAccountTypeLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        myAccountTypeLabel.setHorizontalAlignment(JLabel.CENTER);
 
-            JLabel myAccountTypeLabel = new JLabel("Pick Your Account Type");
-            myAccountTypeLabel.setFont(new Font("Arial", Font.BOLD, 16));
-            myAccountTypeLabel.setHorizontalAlignment(JLabel.CENTER);
 
+        JPanel buttonPanel = new JPanel(new FlowLayout()); // Use FlowLayout for button panel
 
-            JPanel buttonPanel = new JPanel(new FlowLayout()); // Use FlowLayout for button panel
-
-            savingsButton= new JButton("Savings Account");
-            savingsButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    withdraw(20000.0);
-
-                }
-            });
-            buttonPanel.add(savingsButton);
-            add(myAccountTypeLabel, BorderLayout.CENTER);
-            add(buttonPanel,BorderLayout.SOUTH);
-        }
-        public void Withdrawal(double amount ){
-            if (amount <= WITHDRAWAL_LIMIT){
-                if (amount <=
+        savingsButton = new JButton("Savings Account");
+        savingsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                withdraw(20000.0);
 
             }
+        });
+        buttonPanel.add(savingsButton);
+        add(myAccountTypeLabel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public void Withdrawal(double amount) {
+        if (amount <= WITHDRAWAL_LIMIT) {
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("withdrawal amount:" + amount);
+                System.out.println("New balance:" + balance);
+
+            } else {
+                System.out.println("Insufficient funds");
+            }
+        }else {
+            System.out.println("Withdrawal limit exceeded");
         }
     }
+}
 
